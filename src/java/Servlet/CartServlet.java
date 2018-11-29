44265dfbc742935a2560ad63a73204b673ac5864
natthapasboom <natthapas.boom@mail.kmutt.ac.th>
2018-11-29 22:26:26 +0700

    [WTF]

--- src/java/Servlet/CartServlet.java
+++ src/java/Servlet/CartServlet.java
@@ -20,23 +20,11 @@ import jpa.model.ShoppingCart;
  */
 public class CartServlet extends HttpServlet {
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession(true);
         ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
-
-        
-        
         if(cart==null){
             session.setAttribute("emptycart", "Your cart is Empty , Please buy Something :D");
         }
@@ -47,24 +35,10 @@ public class CartServlet extends HttpServlet {
 
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
