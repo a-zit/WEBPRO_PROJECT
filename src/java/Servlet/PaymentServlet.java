d8bd557882103ac870639920336c83bec9b3ef07
natthapasboom <natthapas.boom@mail.kmutt.ac.th>
2018-11-29 19:56:01 +0700

    [UPDATE]PaymentServlet

--- src/java/Servlet/PaymentServlet.java
+++ src/java/Servlet/PaymentServlet.java
@@ -21,7 +21,7 @@ public class PaymentServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        getServletContext().getRequestDispatcher("/payment.jsp").forward(request, response);
     }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
