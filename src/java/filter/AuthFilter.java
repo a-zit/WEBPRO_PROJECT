c41f145011ab53bdec60131776ca18f3df4991d9
natthapasboom <natthapas.boom@mail.kmutt.ac.th>
2018-11-30 00:52:51 +0700

    [update]filter

---
+++ src/java/filter/AuthFilter.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filter;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import jpa.model.Customer;
+
+/**
+ *
+ * @author DEMO TEST
+ */
+public class AuthFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest)request;
+        HttpSession session = httpServletRequest.getSession(true);
+        Customer account = (Customer)session.getAttribute("account");
+        if(account != null) {
+            chain.doFilter(request, response);
+        } else {
+            ((HttpServletResponse)response).sendRedirect("Login");
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+    
+}
